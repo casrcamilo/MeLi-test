99cb2e12bc743c4f10fd1475b2e85a5f8fcba603
Camilo Sosa <casrcamilo@gmail.com>
2023-10-06 17:46:21 -0500

    remove dist folder

--- api/dist/controllers/itemController.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getItems = void 0;
-const itemService_1 = require("../services/itemService");
-const getItems = (req, res) => {
-    const items = (0, itemService_1.getItemList)();
-    res.json(items);
-};
-exports.getItems = getItems;
